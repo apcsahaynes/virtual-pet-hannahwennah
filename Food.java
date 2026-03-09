f07f599609bd9e8979dd2e351f17d5358a1ddcec
hannah <hannah.wen@jpschools.org>
2026-03-09 00:24:29 -0500

    updated Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,19 @@
 public class Food {
-    
+  private String name;
+  private int energy;
+  private int happiness; // may be negative
+  private int weight;
+
+  public Food(String name, int energy, int happiness, int weight) {
+    this.name = name;
+    this.energy = energy;
+    this.happiness = happiness;
+    this.weight = weight;
+  }
+  
+  // getters
+  public int getEnergy() { return energy; }
+  public int getHappiness() { return happiness; }
+  public String getName() { return name; }
+  public int getWeight() { return weight; }
 }
