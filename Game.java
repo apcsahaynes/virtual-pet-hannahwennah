4bd63eaf3d1cb8a9316ece656f1e7fe29b4d772f
hannah <hannah.wen@jpschools.org>
2026-03-09 00:24:47 -0500

    updated Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,21 @@
 public class Game {
+    private String name;
+    private int happiness;
+    private int weight;
+   
+    public Game(String name, int happiness, int weight) {
+        this.name = name;
+        this.happiness = happiness;
+        this.weight = weight;
+    }
     
+    public boolean isWinner() {
+        int temp = (int) (2 * Math.random()); // temp can be 0 or 1
+        return temp == 0;
+    }
+    
+    // getters
+    public int getHappiness() { return happiness; }
+    public String getName() { return name; }
+    public int getWeight() { return weight; }
 }
